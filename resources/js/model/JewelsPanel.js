016fb6e5b98f048f19ecadd0aaed1d3897a22def
NgoMinh <NgoWilliam94@gmail.com>
2014-02-11 00:00:38 +0100

    Init the table with at least one solution

--- resources/js/model/JewelsPanel.js
+++ resources/js/model/JewelsPanel.js
@@ -5,9 +5,8 @@ function JewelsPanel(){
 	for(var i = 0; i < this.jewels.length; i++){
 		this.jewels[i] = new Array(JewelsPanel.NUMBER_OF_COLUMN);
 	}
-
-	this.jewelsType = ['TOPAZ','EMERALD','SAPPHIRE','AMETHYST','RUBY'];
 	
+	this.jewelsType = ['TOPAZ','EMERALD','SAPPHIRE','AMETHYST','RUBY'];
 }
 
 JewelsPanel.NUMBER_OF_LINE    = 9;
@@ -22,37 +21,209 @@ JewelsPanel.constructor = JewelsPanel;
  */
 JewelsPanel.prototype.init = function(){
 	//Complete each line of the array
+	do
+	{
+		for(var idLine = 0; idLine < JewelsPanel.NUMBER_OF_LINE; idLine++)
+		{
+			for(var idColumn = 0; idColumn < JewelsPanel.NUMBER_OF_COLUMN; idColumn++)
+			{
+				do{
+					//Complete the array of jewel with a random jewelsType
+					var jewelsType = this.jewelsType[Math.floor(Math.random()*5)];
+					this.jewels[idLine][idColumn] = new Jewel(jewelsType);
+					//Start the validation at this value
+					var validAt = JewelsPanel.NB_JEWELS_ALIGNED - 2;
+					if(idColumn > validAt)
+					{
+						var resultLine   = this.checkLine(idColumn, idLine);
+					}
+
+					if(idLine > validAt)
+					{
+						var resultColumn = this.checkColumn(idColumn, idLine);
+					}
+				}while(resultLine || resultColumn);
+
+			}
+		}
+	}while(this.checkSolution() !== true);
+}
+
+JewelsPanel.prototype.checkSolution = function() {
+	var tab_jewels_copy = this.createCopy();
 	for(var idLine = 0; idLine < JewelsPanel.NUMBER_OF_LINE; idLine++)
 	{
 		for(var idColumn = 0; idColumn < JewelsPanel.NUMBER_OF_COLUMN; idColumn++)
 		{
-			do{
-				//Complete the array of jewel with a random jewelsType
-				var jewelsType = this.jewelsType[Math.floor(Math.random()*5)];
-				this.jewels[idLine][idColumn] = new Jewel(jewelsType);
-				//Start the validation at this value
-				var validAt = JewelsPanel.NB_JEWELS_ALIGNED-2;
-				if(idColumn > validAt)
+			if(idLine > 0)
+			{
+				this.swap(tab_jewels_copy[idLine][idColumn], tab_jewels_copy[idLine-1][idColumn], tab_jewels_copy);
+				if(this.getJewelsInARow(tab_jewels_copy).length > 0)
 				{
-					var resultLine   = this.checkLine(idColumn, idLine);
+					//console.log('L-1      idLine :'+idLine+'          idColumn'+idColumn);
+					return true;
 				}
-
-				if(idLine > validAt)
+				this.swap(tab_jewels_copy[idLine][idColumn], tab_jewels_copy[idLine-1][idColumn], tab_jewels_copy);
+			}
+			if(idColumn > 0)
+			{
+				this.swap(tab_jewels_copy[idLine][idColumn], tab_jewels_copy[idLine][idColumn-1], tab_jewels_copy);
+				if(this.getJewelsInARow(tab_jewels_copy).length > 0)
 				{
-					var resultColumn = this.checkColumn(idColumn, idLine);
+					//console.log('C-1      idLine :'+idLine+'          idColumn'+idColumn);
+					return true;
 				}
-			}while(resultLine || resultColumn);
-			var validAt = JewelsPanel.NB_JEWELS_ALIGNED-2;
-			if(idColumn > validAt){
-				this.checkLine(idColumn, idLine);
+				this.swap(tab_jewels_copy[idLine][idColumn], tab_jewels_copy[idLine][idColumn-1], tab_jewels_copy);
 			}
-			if(idLine > validAt){
-				this.checkColumn(idColumn, idLine);
-			}
-
 		}
 	}
-}
+	return false;
+};
+
+/**
+ * return array with all jewel in a row
+ */
+JewelsPanel.prototype.getJewelsInARow = function(tab) {
+	var j_delete_list = new Array();
+	//Add aligned jewel on the same line
+	for(var idLine = 0; idLine < JewelsPanel.NUMBER_OF_LINE; idLine++)
+	{
+		var j_type = tab[idLine][0].getType();
+		var j_list = [];
+		j_list.push(tab[idLine][0]);
+		for(var idColumn = 1; idColumn < JewelsPanel.NUMBER_OF_COLUMN; idColumn++)
+		{
+			if(j_type == tab[idLine][idColumn].getType())
+			{
+				j_list.push(tab[idLine][idColumn]);
+			}
+			else
+			{
+				if( j_list.length >= JewelsPanel.NB_JEWELS_ALIGNED )
+				{
+					console.log('number of jewels in a row same line : '+j_list.length);
+					for(var i = 0; i < j_list.length ; i++)
+					{
+						if( jQuery.inArray(j_list[i], j_delete_list) == -1 )
+						{
+							j_delete_list.push(j_list[i]);
+						}	
+					}
+				}
+				j_list.length = 0;
+				j_type        = tab[idLine][idColumn].getType();
+				j_list.push(tab[idLine][idColumn]);
+			}
+		}
+		if( j_list.length >= JewelsPanel.NB_JEWELS_ALIGNED )
+		{
+			console.log('number of jewels in a row same column : '+j_list.length);
+			for(var i = 0; i < j_list.length ; i++)
+			{
+				if( jQuery.inArray(j_list[i], j_delete_list) == -1 )
+				{
+					j_delete_list.push(j_list[i]);
+				}
+			}
+		}
+	}
+
+	//add aligned jewel on the same column
+	for(var idColumn = 0; idColumn < JewelsPanel.NUMBER_OF_COLUMN; idColumn++)
+	{	
+		var j_type = tab[0][idColumn].getType();
+		var j_list = [];
+		j_list.push(tab[0][idColumn]);
+		for(var idLine = 1; idLine < JewelsPanel.NUMBER_OF_LINE; idLine++)
+		{
+			if(j_type == tab[idLine][idColumn].getType())
+			{
+				j_list.push(tab[idLine][idColumn]);
+			}
+			else
+			{
+				if( j_list.length >= JewelsPanel.NB_JEWELS_ALIGNED )
+				{
+					console.log('number of jewels in a row same column : '+j_list.length);
+					for(var i = 0; i < j_list.length ; i++)
+					{
+						if( jQuery.inArray(j_list[i], j_delete_list) == -1 )
+						{
+							j_delete_list.push(j_list[i]);
+						}	
+					}
+				}
+				j_list.length = 0;
+				j_type        = tab[idLine][idColumn].getType();
+				j_list.push(tab[idLine][idColumn]);
+			}
+		}
+		if( j_list.length >= JewelsPanel.NB_JEWELS_ALIGNED )
+		{
+			console.log('number of jewels in a row same column : '+j_list.length);
+			for(var i = 0; i < j_list.length ; i++)
+			{
+				if( jQuery.inArray(j_list[i], j_delete_list) == -1 )
+				{
+					j_delete_list.push(j_list[i]);
+				}	
+			}
+		}
+	}
+
+	return j_delete_list;
+};
+
+JewelsPanel.prototype.createCopy = function() {
+	var copy_jewels = new Array();
+	for(var idLine = 0; idLine < JewelsPanel.NUMBER_OF_LINE; idLine++)
+	{
+		copy_jewels[idLine] = new Array();
+		for(var idColumn = 0; idColumn < JewelsPanel.NUMBER_OF_COLUMN; idColumn++)
+		{
+			copy_jewels[idLine].push(new Jewel(this.jewels[idLine][idColumn].getType()));
+		}
+	}
+	return copy_jewels;
+};
+
+JewelsPanel.prototype.removeJewels = function(array) {
+	for(var i = 0; i < array.length; i++)
+	{
+		this.remove(array[i]);
+	}
+};
+
+JewelsPanel.prototype.remove = function(jewel){
+	var id = this.jewels.indexOf(jewel);
+	this.jewels.remove(id);
+};
+
+JewelsPanel.prototype.swap = function(jewel1, jewel2, tab){
+	
+	var jewel1_L = this.getLine(jewel1, tab);
+	var jewel1_C = this.getColumn(jewel1, tab);
+	var jewel2_L = this.getLine(jewel2, tab);
+	var jewel2_C = this.getColumn(jewel2, tab);
+
+	tab[jewel1_L][jewel1_C] = jewel2;
+	tab[jewel2_L][jewel2_C] = jewel1;
+};
+
+JewelsPanel.prototype.getLine = function(jewel, tab) {
+	for(var idLine = 0; idLine < JewelsPanel.NUMBER_OF_LINE ; idLine++)
+	{
+		if(jQuery.inArray(jewel, tab[idLine]) != -1)
+		{
+			var line = idLine;
+		}
+	}
+	return line;
+};
+
+JewelsPanel.prototype.getColumn = function(jewel, tab) {
+	return tab[this.getLine(jewel, tab)].indexOf(jewel);
+};
 
 
 /**
@@ -60,7 +231,7 @@ JewelsPanel.prototype.init = function(){
  */
 JewelsPanel.prototype.checkColumn = function(idColumn, idLine){
 	//We check the N previous ID if they are the same
-	var nPreviousId = JewelsPanel.NB_JEWELS_ALIGNED-1;
+	var nPreviousId = JewelsPanel.NB_JEWELS_ALIGNED - 1 ;
 	for(var i = 1; i <= nPreviousId; i++){
 
 		if(this.jewels[idLine][idColumn].getType() !== this.jewels[idLine-i][idColumn].getType()){
@@ -76,7 +247,7 @@ JewelsPanel.prototype.checkColumn = function(idColumn, idLine){
  */
 JewelsPanel.prototype.checkLine = function(idColumn, idLine){
 	//We check the N previous ID if they are the same
-	var nPreviousId = JewelsPanel.NB_JEWELS_ALIGNED-1;
+	var nPreviousId = JewelsPanel.NB_JEWELS_ALIGNED - 1 ;
 	for(var i = 1; i <= nPreviousId; i++){
 
 		if(this.jewels[idLine][idColumn].getType() !== this.jewels[idLine][idColumn-i].getType()){
@@ -90,16 +261,16 @@ JewelsPanel.prototype.checkLine = function(idColumn, idLine){
 /**
  * Show the state of the console in the table
  */
-JewelsPanel.prototype.showContent = function() {
+JewelsPanel.prototype.showContent = function(tab) {
 	for(var idLine = 0; idLine < JewelsPanel.NUMBER_OF_LINE; idLine++)
 	{
 		var stringContent = "";
 		for(var idColumn = 0; idColumn < JewelsPanel.NUMBER_OF_COLUMN; idColumn++)
 		{
-			var type   = this.jewels[idLine][idColumn].getType();
+			var type   = tab[idLine][idColumn].getType();
 
 			//Complete the string type with space for a better display in console
-			var length = this.jewels[idLine][idColumn].getType().length;
+			var length = tab[idLine][idColumn].getType().length;
 			if(length < 8){
 				var delta = 8 - length;
 				for (var x = 0; x < delta; x++){
