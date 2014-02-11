592402514297e3cd203c01f44dbf2c768d720a6a
NgoMinh <NgoWilliam94@gmail.com>
2014-02-11 21:39:06 +0100

    add jewelsPanelController attribute and update function

--- resources/js/GameScene.js
+++ resources/js/GameScene.js
@@ -2,10 +2,15 @@ function GameScene(){
 	Scene.call(this);
 	this.jewelsPanel = new JewelsPanel();
 	this.jewelsPanel.init();
-	this.jewelsPanelRenderer = new JewelsPanelRenderer(this.jewelsPanel, this);
+	this.jewelsPanelRenderer   = new JewelsPanelRenderer(this.jewelsPanel, this);
+	this.jewelsPanelController = new JewelsPanelController(this.jewelsPanel, this.jewelsPanelRenderer);
 	this.setBackgroundColor(0x4DC0CA);
 	this.setInteractive(true);
 }
 
 GameScene.constructor = GameScene;
 GameScene.prototype   = Object.create(Scene.prototype);
+
+GameScene.prototype.update = function() {
+	this.jewelsPanelRenderer.update();
+};
